d44ec39517ab38bca14af5fbf7b55e8abb385271
AnupaAlwis <isurajithalwis@gmail.com>
2024-06-12 03:24:04 +0530

    Login Page

---
+++ LibrarySystem/BITProject/BITProject/LogInpage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BITProject
+{
+    public partial class LogInpage : Form
+    {
+        public LogInpage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInpage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
