d6413623c196a790fd7940986254a1e72ec90640
AnupaAlwis <isurajithalwis@gmail.com>
2024-06-13 02:20:21 +0530

    Search Page Completed

--- WindowsFormsApp1/WindowsFormsApp1/searchPage.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/searchPage.Designer.cs
@@ -36,6 +36,10 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -43,7 +47,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label1.Location = new System.Drawing.Point(325, 81);
+            this.label1.Location = new System.Drawing.Point(325, 147);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 25);
             this.label1.TabIndex = 3;
@@ -54,7 +58,7 @@
             this.textBox1.BackColor = System.Drawing.Color.LightYellow;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBox1.Location = new System.Drawing.Point(281, 35);
+            this.textBox1.Location = new System.Drawing.Point(281, 101);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(246, 30);
             this.textBox1.TabIndex = 2;
@@ -66,7 +70,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label2.Location = new System.Drawing.Point(325, 213);
+            this.label2.Location = new System.Drawing.Point(325, 253);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(138, 25);
             this.label2.TabIndex = 5;
@@ -77,7 +81,7 @@
             this.textBox2.BackColor = System.Drawing.Color.LightYellow;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBox2.Location = new System.Drawing.Point(281, 168);
+            this.textBox2.Location = new System.Drawing.Point(281, 208);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(246, 30);
             this.textBox2.TabIndex = 4;
@@ -89,7 +93,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label3.Location = new System.Drawing.Point(325, 339);
+            this.label3.Location = new System.Drawing.Point(325, 379);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(143, 25);
             this.label3.TabIndex = 7;
@@ -100,7 +104,7 @@
             this.textBox3.BackColor = System.Drawing.Color.LightYellow;
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBox3.Location = new System.Drawing.Point(281, 291);
+            this.textBox3.Location = new System.Drawing.Point(281, 331);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(246, 30);
             this.textBox3.TabIndex = 6;
@@ -110,7 +114,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(156, 394);
+            this.button1.Location = new System.Drawing.Point(33, 394);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(156, 32);
             this.button1.TabIndex = 8;
@@ -121,7 +125,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(484, 394);
+            this.button2.Location = new System.Drawing.Point(583, 394);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(156, 32);
             this.button2.TabIndex = 9;
@@ -129,12 +133,48 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(347, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Enter ID";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox4.Location = new System.Drawing.Point(281, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(246, 30);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Eter ID4";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 128);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 12;
+            // 
             // searchPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -146,6 +186,7 @@
             this.Name = "searchPage";
             this.Text = "Search";
             this.Load += new System.EventHandler(this.searchPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,5 +202,8 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/searchPage.cs
+++ WindowsFormsApp1/WindowsFormsApp1/searchPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
     public partial class searchPage : Form
     {
+        string server = "localhost";
+        string user = "root";
+        string password = "";
+        string database = "librarymanagementsystem";
         public searchPage()
         {
             InitializeComponent();
@@ -24,8 +30,8 @@ namespace WindowsFormsApp1
             func = (controls) =>
             {
                 foreach (Control control in controls)
-                    if (control is TextBox)
-                        (control as TextBox).Clear();
+                    if (control is System.Windows.Forms.TextBox)
+                        (control as System.Windows.Forms.TextBox).Clear();
                     else
                         func(control.Controls);
             };
@@ -40,7 +46,38 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string connectionString = "server=" + server + ";user=" + user + ";password=" + password + ";database=" + database + ";";
+            MySqlConnection con = new MySqlConnection(connectionString);
+            con.Open();
+            string searchQuery = @"
+                SELECT * FROM books
+                WHERE (@book_id IS NULL OR book_id = @book_id)
+                AND (@book_name IS NULL OR book_name = @book_name)
+                AND (@book_isbn IS NULL OR book_isbn = @book_isbn)
+                AND (@book_author IS NULL OR book_author = @book_author)";
 
+            using (MySqlCommand cmd = new MySqlCommand(searchQuery, con))
+            {
+                cmd.Parameters.AddWithValue("@book_id", string.IsNullOrEmpty(textBox4.Text) ? (object)DBNull.Value : textBox4.Text);
+                cmd.Parameters.AddWithValue("@book_name", string.IsNullOrEmpty(textBox1.Text) ? (object)DBNull.Value : textBox1.Text);
+                cmd.Parameters.AddWithValue("@book_isbn", string.IsNullOrEmpty(textBox3.Text) ? (object)DBNull.Value : textBox3.Text);
+                cmd.Parameters.AddWithValue("@book_author", string.IsNullOrEmpty(textBox2.Text) ? (object)DBNull.Value : textBox2.Text);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                if (dataTable.Rows.Count > 0)
+                {
+                    dataGridView1.DataSource = dataTable;
+                }
+                else
+                {
+                    MessageBox.Show("No book found with the provided details.");
+                    dataGridView1.DataSource = null;
+                }
+            }
+            con.Close();
         }
 
         private void button2_Click(object sender, EventArgs e)
